94151c0ee5cdd0ad4c8e6dff9d0cb8b7ecf7039e
Mohammadreza <mrzfvl3908@gmail.com>
2026-08-18 02:04:52 +0330

    add home_app and add templates in index

--- Core/Core/urls.py
+++ Core/Core/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from Core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home_app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
