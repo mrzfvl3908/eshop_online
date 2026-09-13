fd40069a311f219c0ac4fee6fe796f8e59379448
Mohammadreza <mrzfvl3908@gmail.com>
2026-09-13 22:34:03 +0330

    edit admin.py and add iphone 17

--- core/product_app/admin.py
+++ core/product_app/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Color,Products,Size
 
 
-admin.site.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['title','price','discount','created_at']
+    list_filter = ['title']
+    search_fields = ['title','description','created_at','price']
+
+
+admin.site.register(Products,ProductsAdmin)
 admin.site.register(Color)
 admin.site.register(Size)
 
