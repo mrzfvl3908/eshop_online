6af556bea6670629534f82a00e32eed8af8cf379
Mohammadreza <mrzfvl3908@gmail.com>
2026-09-15 18:44:42 +0330

    update home_app

--- core/home_app/urls.py
+++ core/home_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'home_app'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeView.as_view(), name='home'),
 ]
--- core/home_app/views.py
+++ core/home_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def home(request):
-    return render(request, 'home_app/index.html', {})
+
+class HomeView(TemplateView):
+    template_name = 'home_app/index.html'
